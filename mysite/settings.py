341f61f338e2c5eae21a8b070c7f0cde985710f9
siddharthcv77 <sc10143@nyu.edu>
2024-03-03 21:58:27 -0500

    reformatted

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-y)8c31pf*xrpv0w4&m-^5k)puz$p0r(zt1d6%#iez)ykn@b=v#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
